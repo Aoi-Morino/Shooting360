d2dc1fb288d921addd80c3ed9a5a08ff0d57b5b1
AoiMorino <157398147+Aoi-Morino@users.noreply.github.com>
2025-01-03 23:47:01 +0900

    HPバー表示位置・描画アルゴリズム修正
    HPバーをPCの頭上に表示されるように修正
    描画アルゴリズムを修正し、マップチップの基本サイズが変更されたときに画像がそのサイズに合わせ拡大・縮小するよう修正

--- gameMain.py
+++ gameMain.py
@@ -1,15 +1,17 @@
 import pygame as pg
 import numpy as np
 import random as r
+import math as m
 
 def Main():
 
   # 初期化処理
   chip_s = 48  # マップチップの基本サイズ
-  map_s = pg.Vector2(32, 17)  # マップの横・縦の配置数
+  map_s = pg.Vector2(1550 / chip_s, 810 / chip_s)  # マップの横・縦の配置数
+  mapRan = chip_s * map_s
 
   pg.init()
-  pg.display.set_caption('ぼくのかんがえたさいきょうのげーむ II')
+  pg.display.set_caption('ゲームテスト')
   disp_w = int(chip_s * map_s.x)
   disp_h = int(chip_s * map_s.y)
   screen = pg.display.set_mode((disp_w, disp_h))
@@ -23,7 +25,7 @@ def Main():
   bulletPos = []
   bulletAddCtrl = False
   bulletDecay = 0.92  # 弾の減衰率(1以上は加速)
-  bulletOrgSpeed = 36
+  bulletOrgSpeed = chip_s * 3 / 4
   bulletSpeed = []
   bulletMaxTime = 50
   bulletTime = []
@@ -32,21 +34,24 @@ def Main():
   bulletDirTemp = []
   bulletDirRecip = 0.0
 
-  HPBarFrame = 4
+  HPBarAllSize = [int(chip_s * 1.3), int(chip_s * 0.13)]
+  HPBarFrame = m.floor(chip_s / 40)
+  barFrameColor = (255, 255, 255)
+  barBackColor = (66, 0, 0)
+  barHPColor = (0, 200, 200)
+  barSizeTemp = (HPBarFrame * 2, HPBarFrame, -1 *
+                 (HPBarFrame * 4), -1 * (HPBarFrame * 2))
 
+  background_s = pg.Vector2(chip_s, chip_s)
   background_img = pg.image.load(f'data/img/map-background.png')
-  background_s = pg.Vector2(48, 48)
+  background_img = pg.transform.scale(background_img, background_s)  # 拡大
 
   # グリッド設定
   grid_cx = '#ff0000'
   grid_cy = '#0000ff'
 
   # 自キャラ移動関連
-  cmdWMove = False  # Wキー移動コマンドの管理変数
-  cmdAMove = False  # Aキー移動コマンドの管理変数
-  cmdSMove = False  # Sキー移動コマンドの管理変数
-  cmdDMove = False  # Dキー移動コマンドの管理変数
-  cmd_move = -1  # 移動コマンドの総合管理変数
+  cmdMove = [False, False, False, False]  # 移動コマンドの総合管理変数
   m_vec = [
       pg.Vector2(0, -0.2),
       pg.Vector2(0.2, 0),
@@ -56,7 +61,7 @@ def Main():
 
   # 自キャラの画像読込み
   reimu_p = pg.Vector2(2, 3)   # 自キャラ位置
-  reimu_s = pg.Vector2(48, 64)  # 画面に出力する自キャラサイズ 48x64
+  reimu_s = pg.Vector2(chip_s, chip_s * 1.5)  # 画面に出力する自キャラサイズをマップチップに合わせる
   reimu_d = 2  # 自キャラの向き
   reimu_img_raw = pg.image.load('./data/img/reimu.png')
   reimu_img_arr = []
@@ -70,13 +75,15 @@ def Main():
     reimu_img_arr[i].append(reimu_img_arr[i][1])
 
     # 弾画像の読み込み
-  bullet = pg.image.load("Data/img/bullet.png")
+  bullet_s = pg.Vector2(chip_s, chip_s)
+  bullet_img = pg.image.load("Data/img/bullet.png")
+  bullet_img = pg.transform.scale(bullet_img, bullet_s)  # 拡大
 
   # 弾の追加
   def BulletAdd():
 
     bulletDirTemp = pg.mouse.get_pos()
-    bulletDirTemp -= (dp + [24, 36])
+    bulletDirTemp -= (dp + [chip_s / 2, chip_s * 3 / 4])
     bulletDirTemp += [r.randint(-bulletMOA, bulletMOA),
                       r.randint(-bulletMOA, bulletMOA)]
     bulletDirRecip = 1 / np.sqrt(bulletDirTemp[0]**2 + bulletDirTemp[1]**2)
@@ -84,7 +91,7 @@ def Main():
       bulletDirTemp[i] *= bulletDirRecip * bulletOrgSpeed
 
     if frame % bulletROF == 0:
-      bulletPos.append(dp + [0, 12])
+      bulletPos.append(dp + [0, chip_s / 4])
       bulletTime.append(bulletMaxTime)
       bulletSpeed.append(bulletDirTemp)
     if len(bulletPos) >= bulletMAX:
@@ -105,6 +112,22 @@ def Main():
         bulletSpeed.pop(0)
         bulletTime.pop(0)
 
+  # HPバーの更新
+  def HPBarUpdate():
+    global barFrameSize
+    global barBackSize
+    global barHPSize
+
+    barPos_x = dp[0] + (chip_s - HPBarAllSize[0]) / 2
+    barPos_y = dp[1] - HPBarAllSize[1]
+    barFrameSize = [barPos_x, barPos_y, HPBarAllSize[0], HPBarAllSize[1]]
+    barBackSize = [0, 0, 0, 0]
+    barHPSize = [0, 0, 0, 0]
+    for i in range(len(barFrameSize)):
+      barBackSize[i] = barFrameSize[i]
+      barBackSize[i] += barSizeTemp[i]
+      barHPSize[i] = barBackSize[i]
+
   # ゲームループ
   while not exit_flag:
 
@@ -116,23 +139,23 @@ def Main():
       # 移動操作の「キー離し」の受け取り処理
       if event.type == pg.KEYUP:
         if event.key == pg.K_w:
-          cmdWMove = False
-        if event.key == pg.K_a:
-          cmdAMove = False
-        if event.key == pg.K_s:
-          cmdSMove = False
+          cmdMove[0] = False
         if event.key == pg.K_d:
-          cmdDMove = False
+          cmdMove[1] = False
+        if event.key == pg.K_s:
+          cmdMove[2] = False
+        if event.key == pg.K_a:
+          cmdMove[3] = False
       # 移動操作の「キー入力」の受け取り処理
       if event.type == pg.KEYDOWN:
         if event.key == pg.K_w:
-          cmdWMove = True
-        if event.key == pg.K_a:
-          cmdAMove = True
-        if event.key == pg.K_s:
-          cmdSMove = True
+          cmdMove[0] = True
         if event.key == pg.K_d:
-          cmdDMove = True
+          cmdMove[1] = True
+        if event.key == pg.K_s:
+          cmdMove[2] = True
+        if event.key == pg.K_a:
+          cmdMove[3] = True
 
       # マウスクリック/クリック離しの受け取り処理
       if event.type == pg.MOUSEBUTTONDOWN:
@@ -143,9 +166,9 @@ def Main():
     # 背景描画
     screen.fill(pg.Color('WHITE'))
 
-    for x in range(0, disp_w, int(background_s.x)):
-      for y in range(0, disp_h, int(background_s.y)):
-        screen.blit(background_img, (x, y))
+    for x in range(0, int(map_s.x) + 1):
+      for y in range(0, int(map_s.y) + 1):
+        screen.blit(background_img, (chip_s * x, chip_s * y))
 
     # グリッド
     for x in range(0, disp_w, chip_s):  # 縦線
@@ -153,30 +176,14 @@ def Main():
     for y in range(0, disp_h, chip_s):  # 横線
       pg.draw.line(screen, grid_cy, (0, y), (disp_w, y))
 
-    # HPバーの描画
-    pg.draw.rect(screen, (255, 255, 255), (48 * 1, 48 * 15, 48 * 6, 48 * 1))
-    pg.draw.rect(screen, (66, 0, 0), (48 * 1 + HPBarFrame * 2,
-                 48 * 15 + HPBarFrame, 48 * 6 - (HPBarFrame * 4), 48 * 1 - (HPBarFrame * 2)))
-    pg.draw.rect(screen, (0, 200, 200), (48 * 1 + HPBarFrame * 2,
-                 48 * 15 + HPBarFrame, 48 * 6 - (HPBarFrame * 4), 48 * 1 - (HPBarFrame * 2)))
-
     # 移動コマンドの処理
-    if cmdWMove == True:
-      cmd_move = 0
-    elif cmdDMove == True:
-      cmd_move = 1
-    elif cmdSMove == True:
-      cmd_move = 2
-    elif cmdAMove == True:
-      cmd_move = 3
-    else:
-      cmd_move = -1
 
-    if cmd_move != -1:
-      reimu_d = cmd_move  # キャラクタの向きを更新
-      af_pos = reimu_p + m_vec[cmd_move]  # 移動後の (仮) 座標
-      if (0 <= af_pos.x <= map_s.x - 1) and (0 <= af_pos.y <= map_s.y - 1):
-        reimu_p += m_vec[cmd_move]  # 画面外に移動しないなら実際のキャラを移動
+    for i in range(len(cmdMove)):
+      if cmdMove[i] == True:
+        reimu_d = i  # キャラクタの向きを更新
+        af_pos = reimu_p + m_vec[i]  # 移動後の (仮) 座標
+        if (0 <= af_pos.x <= map_s.x - 1) and (0 <= af_pos.y <= map_s.y - 1):
+          reimu_p += m_vec[i]  # 画面外に移動しないなら実際のキャラを移動
 
     # 弾の生成
     if bulletAddCtrl == True:
@@ -189,10 +196,16 @@ def Main():
 
     # 弾の描画
     for i in range(len(bulletPos)):
-      screen.blit(bullet, bulletPos[i])
+      screen.blit(bullet_img, bulletPos[i])
 
     # 弾の移動
     BulletMove()
+
+    # HPバーの描画
+    HPBarUpdate()
+    pg.draw.rect(screen, barFrameColor, barFrameSize)
+    pg.draw.rect(screen, barBackColor, barBackSize)
+    pg.draw.rect(screen, barHPColor, barHPSize)
 
     # フレームカウンタの描画
     frame += 1
